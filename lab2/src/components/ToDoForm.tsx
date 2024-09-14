1980896a7353d5860719512081ab0dcac63e951e
taimg1 <oleg.s2822@gmail.com>
2024-09-14 22:15:49 +0300

    upd

--- lab2/src/components/ToDoForm.tsx
+++ lab2/src/components/ToDoForm.tsx
@@ -11,7 +11,7 @@ const ToDoForm: React.FC<Props> = ({toDo, handleAddToDo, handleChange}) => {
     return (
         <form onSubmit={handleAddToDo}>
             <input name="title" value={toDo.title} onChange={handleChange} type="text" placeholder="Title"/>
-            <input name="completed" value={toDo.completed} onChange={handleChange} type="checkbox"/>
+            <input name="completed" value={toDo.completed ? 'true' : 'false'} onChange={handleChange} type="checkbox"/>
             <button type="submit">Add</button>
         </form>
     )
